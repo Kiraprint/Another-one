694dba7663bc42c41046f7d69210d2e2bde4a7d4
Kiraprint <76822921+Kiraprint@users.noreply.github.com>
2021-01-06 12:36:31 +0300

    2.0

--- yep.py
+++ yep.py
@@ -1,7 +1,7 @@
 import sys
 
 import random as r
-from PyQt5 import uic
+from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QMainWindow, QApplication
 
@@ -10,10 +10,37 @@ def except_hook(cls, exception, traceback):
     sys.__excepthook__(cls, exception, traceback)
 
 
-class MyWidget(QMainWindow):
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(340, 260, 93, 28))
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 26))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Yo"))
+
+
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('Ui.ui', self)
+        self.setupUi(self)
         self.pushButton.clicked.connect(self.draw)
         self.do_paint = False
 
@@ -25,7 +52,7 @@ class MyWidget(QMainWindow):
         self.do_paint = False
 
     def draw_ell(self, qp):
-        qp.setBrush(QColor('yellow'))
+        qp.setBrush(QColor(r.randrange(255), r.randrange(255), r.randrange(255)))
         rad = r.randrange(self.geometry().x())
         qp.drawEllipse(r.randrange(self.geometry().x()), r.randrange(self.geometry().y()), rad, rad)
 
